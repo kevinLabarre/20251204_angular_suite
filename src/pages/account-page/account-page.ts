30c91d5a9ff00312396e53ce6e6160646486c6da
klabarre <klabarre@dawan.fr>
2025-12-05 09:19:09 +0100

    ajout du routerlinkactive sur notre 'menu' . AJout d'un db.json pour le json-server

--- src/pages/account-page/account-page.ts
+++ src/pages/account-page/account-page.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './account-page.html',
   styleUrl: './account-page.css',
 })
